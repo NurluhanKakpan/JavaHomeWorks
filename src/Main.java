081ff8e2d116925c7c83915837a9aa1218813abc
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-04-03 09:35:50 +0600

    this problem about Fibonacci numbers,first I will check,if number equals to 0 or 1 and after that I will call recursion function and this return sum of two function that props number-1 and number-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println(Fib(a));
     }
+    public  static  int Fib(int a){
+        if(a==0){
+            return 0;
+        } else if (a==1) {
+            return 1;
+        }
+        return Fib(a-1) + Fib(a-2);
+    }
+
 }
