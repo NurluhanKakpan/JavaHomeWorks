8584db9df1b0cb56d83b3fb641dc6ba9240ebf4c
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-04-05 08:10:04 +0600

    i  use recursion and if number equals to 0 or 1 function returns 1 else returns n*factorial(n-1)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 4;
-        int[] arr = {1,4,6,2};
-        reverse(arr,0,a);
-        for (int j : arr) {
-            System.out.print(j + " ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(factorial(n));
+
+        
+
+
+    }
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+        } else if (n==1) {
+            return 1;
         }
-
+        return n*factorial(n-1);
     }
-    public static void reverse(int[] arr, int begin, int end  ){
-        int temp;
-            if(begin >=end){
-            }
-           else{
-               temp = arr[begin];
-               arr[begin] = arr[end];
-               arr[end] = temp;
-               reverse(arr,begin+1,end-1);
-            }
-    }
-
-
-
-
 
 }
